af9ca3b4a35d03f1eb67667deffb433250ae41f2
Mytheus <matheussantana2603@gmail.com>
2025-04-17 18:55:11 -0300

    Added problem selection interface

--- LPProblems/main.cpp
+++ LPProblems/main.cpp
@@ -1,8 +1,41 @@
 #include <ilcplex/ilocplex.h>
 #include "Problems.hpp"
-
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+using namespace std;
 
 int main() {
-    PlantingProblem();
-    return 0;
+    const vector<string> PROBLEMS= {
+        "RationProblem",
+        "DietProblem",
+        "PlantingProblem"
+    };
+    const int NUM_PROBLEMS = PROBLEMS.size();
+    vector<function<void(void)>> functions = {RationProblem, DietProblem, PlantingProblem};
+    while (true) {
+        int problemChoice;
+        cout << "Escolha o problema para executar:\n";
+        for (int i = 0; i < NUM_PROBLEMS; i++) {
+            cout << i + 1 << ". " << PROBLEMS[i] << endl;
+        }
+        cout << NUM_PROBLEMS + 1 << ". Sair\n";
+        cout << "Digite o número do problema: ";
+        cin >> problemChoice;
+
+        if (problemChoice < 1 || problemChoice > NUM_PROBLEMS + 1) {
+            cout << "Opção inválida. Tente novamente.\n";
+            continue;
+        }
+        else if (problemChoice == NUM_PROBLEMS + 1) {
+            cout << "Saindo...\n";
+            break;
+        }
+
+        else {
+            cout << "Executando " << PROBLEMS[problemChoice - 1] << "...\n";
+            functions[problemChoice - 1]();
+        }
+    }
 }
